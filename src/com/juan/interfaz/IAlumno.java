83a2d02c1e40a89ec815ae5687db9e5ce6ee5327
Walter Pagani <paganiwalter@gmail.com>
2021-04-15 23:15:00 -0300

    Documentacion en implementacion de Alumno (#2)
    Se agregan lineas de comentarios, sobre los metodos para decir que es lo que hacen.

--- src/com/juan/interfaz/IAlumno.java
+++ src/com/juan/interfaz/IAlumno.java
@@ -5,23 +5,33 @@ import com.juan.clases.Materia;
 
 public interface IAlumno {
 
+	/* Retorna la libreta del alumno */
 	public int getLibreta();
 
+	/* Modifica la libreta del alumno */
 	public void setLibreta(int libreta);
 
+	/* Retorna el nombre del alumno */
 	public String getNombre();
 
+	/* Modifica el nombre del alumno */
 	public void setNombre(String nombre);
 
-	public String getApellido(); 
-	
-	public void setApellido(String apellido) ;
+	/* Retorna el apellido del alumno */
+	public String getApellido();
 
+	/* Modifica el apellido del alumno */
+	public void setApellido(String apellido);
+
+	/* Retorna la lista de materias del alumno */
 	public ArrayList<Materia> getMaterias();
-	
+
+	/* Agrega una materia a la lista del alumno */
 	public void agregarMateria(String nombre, int nota);
 
+	/* Verifica si la nota es mayor igual a 4 y menor o igual a 10 */
 	public boolean estaAprobada (int nota);  
 
+	/* Calcula el promedio de las notas del alumno */
 	public int calcularPromedio();
 }
